7ce390cc6f773063e5f7b7a51b76116ea16daf5b
Chisomo Wisck <38434712+chisomowisck@users.noreply.github.com>
2023-04-15 12:53:09 +0200

    Updated Config files

--- auth_db.sql
+++ auth_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2023 at 11:57 AM
+-- Generation Time: Apr 15, 2023 at 12:49 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -29,8 +29,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `roles` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL
+  `name` varchar(255) DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
+(1, 'user', '2023-04-15 10:37:40', '2023-04-15 10:37:40'),
+(2, 'admin', '2023-04-15 10:37:40', '2023-04-15 10:37:40'),
+(3, 'moderator', '2023-04-15 10:37:40', '2023-04-15 10:37:40');
 
 -- --------------------------------------------------------
 
@@ -42,8 +53,38 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
+  `password` varchar(255) DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `createdAt`, `updatedAt`) VALUES
+(5, 'chisomo', 'chisomowisck@gmail.com', '$2a$08$/l/C3.faXHgEM00RSyxST.PgGUnIGd53LBYJBMi4RAtgag0Yb8Baq', '2023-04-15 10:46:32', '2023-04-15 10:46:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `userId` int(10) NOT NULL,
+  `roleId` int(10) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+INSERT INTO `user_roles` (`userId`, `roleId`, `createdAt`, `updatedAt`) VALUES
+(5, 1, '2023-04-15 10:46:32', '2023-04-15 10:46:32'),
+(5, 2, '2023-04-15 10:46:32', '2023-04-15 10:46:32');
 
 --
 -- Indexes for dumped tables
@@ -62,6 +103,12 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`userId`,`roleId`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -69,7 +116,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
